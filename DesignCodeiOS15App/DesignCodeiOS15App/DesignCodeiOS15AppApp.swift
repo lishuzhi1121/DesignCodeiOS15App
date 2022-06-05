53b5dc770320aa0bcbc1593ff3e083683709c6ba
SandsLee <lishuzhi1121@163.com>
2022-06-05 18:26:03 +0800

    init project

---
+++ DesignCodeiOS15App/DesignCodeiOS15App/DesignCodeiOS15AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15AppApp.swift
+//  DesignCodeiOS15App
+//
+//  Created by SandsLee on 2022/6/5.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
