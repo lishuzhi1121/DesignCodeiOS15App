fe157726c10645b69e61debc6cfa89b686248f6f
SandsLee <lishuzhi1121@163.com>
2022-06-05 19:24:29 +0800

    Visual Editor

--- DesignCodeiOS15App/DesignCodeiOS15App/ContentView.swift
+++ DesignCodeiOS15App/DesignCodeiOS15App/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("SwiftUI for iOS 15")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .padding(.vertical)
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+        .padding(.all, 20.0)
+        .frame(height: 350.0)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
     }
 }
 
