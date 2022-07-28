5fedaff8864a4794baf35aaeaa6a8c6b9f3936f6
SandsLee <lishuzhi1121@163.com>
2022-07-28 15:19:42 +0800

    Style, Shape and AccountView

---
+++ DesignCodeiOS15App/DesignCodeiOS15App/AccountView.swift
@@ -0,0 +1,96 @@
+//
+//  AccountView.swift
+//  DesignCodeiOS15App
+//
+//  Created by SandsLee on 2022/7/28.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                VStack(spacing: 8) {
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundStyle(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                    )
+                    Text("Meng To")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.medium)
+                        Text("China")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                
+                Section {
+                    NavigationLink {
+                        ContentView()
+                    } label: {
+                        Label("Settings", systemImage: "gear")
+                    }
+                    NavigationLink {
+                        Text("Billing")
+                    } label: {
+                        Label("Billing", systemImage: "creditcard")
+                    }
+                    NavigationLink {
+                        Text("Help")
+                    } label: {
+                        Label("Help", systemImage: "questionmark")
+                    }
+                }
+                .accentColor(.primary)
+                .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+                
+                Section {
+                    Link(destination: URL(string: "https://apple.com/")!) {
+                        HStack {
+                            Label("Website", systemImage: "house")
+                            Spacer()
+                            Image(systemName: "link")
+                                .imageScale(.small)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    Link(destination: URL(string: "https://youtube.com/")!) {
+                        HStack {
+                            Label("YouTube", systemImage: "tv")
+                            Spacer()
+                            Image(systemName: "link")
+                                .imageScale(.small)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .accentColor(.primary)
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
